aa987b1e053dce573c98aa1b8dab0cf30d0f7e7b
Cauê Thenório <cauelt@gmail.com>
2015-11-19 23:03:16 -0200

    Add i3 config files, set config.sh autojump config osx and linux compatible, some sublime path dir fix

--- general/.config/fish/config.fish
+++ general/.config/fish/config.fish
@@ -1,1 +1,6 @@
-. /usr/share/autojump/autojump.fish
+# load autojump
+
+for AUTOJUMP_DIR in /usr/share/autojump \
+                    /usr/local/share/autojump
+    test -e $AUTOJUMP_DIR/autojump.fish; and . $AUTOJUMP_DIR/autojump.fish; or true
+end
